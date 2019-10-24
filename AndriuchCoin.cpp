82620924c3341a4f6fc918156593f98b15c163bf
Andriusjok <andriusjokubauskass@gmail.com>
2019-10-24 22:35:45 +0300

    0.2 be transNuskaitymo

--- AndriuchCoin.cpp
+++ AndriuchCoin.cpp
@@ -10,6 +10,7 @@
 #include <chrono>
 #include <time.h>
 #include <list>
+#include <algorithm>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -20,17 +21,34 @@ using std::list;
 class transaction
 {
 private:
-	unsigned int number;
 	string SenderKey;
 	string ReceiverKey;
 	float amount;
+	string transID;
 public:
-	transaction(unsigned int number_, string SenderKey_, string  ReceiverKey_, float amount_): number(number_), SenderKey(SenderKey_), ReceiverKey(ReceiverKey_), amount(amount_) {}
+	transaction(string SenderKey_, string  ReceiverKey_, float amount_, string transID_): SenderKey(SenderKey_), ReceiverKey(ReceiverKey_), amount(amount_), transID(transID_) {}
 
-void printtrans()
+
+	void printtrans()
 	{
-		cout<<"nr "<<number<<" Skey "<<SenderKey<<" rkey "<<ReceiverKey<<" amt "<<amount<<endl;
-		cout<<endl;
+		cout << " Skey " << SenderKey << " rkey " << ReceiverKey << " amt " << amount << " transID " << transID << endl;
+		cout << endl;
+	}
+	string getSkey()
+	{
+		return SenderKey;
+	}
+	string getRkey()
+	{
+		return ReceiverKey;
+	}
+	string getamount()
+	{
+		return (std::to_string(amount));
+	}
+	string getID()
+	{
+		return transID;
 	}
 };
 
@@ -46,12 +64,16 @@ public:
 	{
 		return publicKey;
 	}
+	float getmoney()
+	{
+		return money;
+	}
 	void printuser()
 	{
-		cout<<userName<<endl;
-		cout<<money<<endl;
-		cout<<publicKey<<endl;
-		cout<<endl;
+		cout << userName << endl;
+		cout << money << endl;
+		cout << publicKey << endl;
+		cout << endl;
 	}
 };
 class block {
@@ -78,44 +100,44 @@ public:
 	}
 	string ReturnHashable()
 	{
-		return (HashBlock+prevHash+TimeStamp+Version+treeHash+std::to_string(nonce)+std::to_string(difficulty)+std::to_string(maxTrans));
+		return (HashBlock + prevHash + TimeStamp + Version + treeHash + std::to_string(nonce) + std::to_string(difficulty) + std::to_string(maxTrans));
 	}
 	void printblock()
 	{
-		cout<<"Bloko hashas"<<endl;
-		cout<<HashBlock<<endl;
-		cout<<"Praeito bloko hashas"<<endl;
-		cout<<prevHash<<endl;
-		cout<<"Data"<<endl;
-		cout<<TimeStamp<<endl;
-		cout<<"Versija"<<endl;
-		cout<<Version<<endl;
-		cout<<"Merkletreehashas (neimplementuota)"<<endl;
-		cout<<treeHash<<endl;
-		cout<<"Nonce su kuriuo pavyko suhashint"<<endl;
-		cout<<nonce<<endl;
-		cout<<"Kiek nuliu reikejo"<<endl;
-		cout<<difficulty<<endl;
-		cout<<"transakcijos"<<endl;
-		for (int i =0 ;i < trans.size();i++)
+		cout << "Bloko hashas" << endl;
+		cout << HashBlock << endl;
+		cout << "Praeito bloko hashas" << endl;
+		cout << prevHash << endl;
+		cout << "Data" << endl;
+		cout << TimeStamp << endl;
+		cout << "Versija" << endl;
+		cout << Version << endl;
+		cout << "Merkletreehashas (neimplementuota)" << endl;
+		cout << treeHash << endl;
+		cout << "Nonce su kuriuo pavyko suhashint" << endl;
+		cout << nonce << endl;
+		cout << "Kiek nuliu reikejo" << endl;
+		cout << difficulty << endl;
+		cout << "transakcijos" << endl;
+		for (int i = 0 ; i < trans.size(); i++)
 		{
 			trans[i].printtrans();
 		}
-		cout<<endl;
+		cout << endl;
 	}
 
 };
-string generate(int max_length){
+string generate(int max_length) {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    string possible_characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#@$^&*()_+-:;,./<>?@|";
+	string possible_characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#@$^&*()_+-:;,./<>?@|";
 	std::default_random_engine generator (seed);
-	std::uniform_real_distribution<double> distribution (0.0,100.0);
-    string ret = "";
-    for(int i = 0; i < max_length; i++){
-        int random_index = distribution(generator); //get index between 0 and possible_characters.size()-1
-        ret += possible_characters[random_index];
-    }
-    return ret;
+	std::uniform_real_distribution<double> distribution (0.0, 100.0);
+	string ret = "";
+	for (int i = 0; i < max_length; i++) {
+		int random_index = distribution(generator); //get index between 0 and possible_characters.size()-1
+		ret += possible_characters[random_index];
+	}
+	return ret;
 }
 
 string Hash(const char* input, unsigned int length)
@@ -159,8 +181,8 @@ void GenerateUsers(size_t amount, vector <user>& accounts)
 	for (int i = 0; i < amount; i++)
 	{
 		string input = generate(16);
-		string name=generate(8);
-		user temp(name,i+100, Hash(input.c_str(), input.length()));
+		string name = generate(8);
+		user temp(name, rand() % 100 + 100, Hash(input.c_str(), input.length()));
 		//temp.printuser();
 		accounts.push_back(temp);
 	}
@@ -171,10 +193,19 @@ void GenerateTransactions(size_t amount, vector <transaction>& transac, vector<u
 	srand(time(NULL));
 	for (int i = 0; i < amount; i++)
 	{
-		string sender = accounts[rand() % 1000].getpublicKey();
-		string receiv = accounts[rand() % 1000].getpublicKey();
-		transaction tempTrans(i, Hash(sender.c_str(), sender.length()), Hash(receiv.c_str(), receiv.length()) , rand() % 100 + 10);
-		transac.push_back(tempTrans);
+		user SendAccount = accounts[rand() % 1000];
+		string sender = SendAccount.getpublicKey();
+		string sendHash = Hash(sender.c_str(), sender.length());
+
+		user ReceivAccount = accounts[rand() % 1000];
+		string receiv = ReceivAccount.getpublicKey();
+		string receivHash = Hash(receiv.c_str(), receiv.length());
+		float amount = rand() % 100 + 10;
+		string ID = sendHash + receivHash + std::to_string(amount);
+		transaction tempTrans(sendHash, receivHash, amount, Hash(ID.c_str(), ID.length()));
+		if (amount <= SendAccount.getmoney())
+			transac.push_back(tempTrans);
+		else cout << "Nepakankamas balansas transakcijai " << amount << " > " << SendAccount.getmoney() << endl;
 		//tempTrans.printtrans();
 	}
 }
@@ -185,28 +216,73 @@ void GenerateBlock(int blockNr, vector<user>& accounts, vector<transaction>& tra
 		int diff = 4;
 		if (blockNr == 0)
 		{
-			vector <transaction> trans;
-			if (transac.size()<100)
-				cout<<"TRUKSTA TRANSAKCIJU"<<endl;
-			while(trans.size()<100)
-			{	
-				trans.push_back(transac.back());
-				transac.pop_back();
+			vector <transaction> transa1;
+			vector <transaction> transb1;
+			vector <transaction> transc1;
+			vector <transaction> transd1;
+			vector <transaction> transe1;
+			if (transac.size() < 100)
+				cout << "TRUKSTA TRANSAKCIJU" << endl;
+			while (transa1.size() < 100)
+			{
+				int a1 = rand() % transac.size();
+				int b1 = rand() % transac.size();
+				int c1 = rand() % transac.size();
+				int d1 = rand() % transac.size();
+				int e1 = rand() % transac.size();
 
+				string check = transac[a1].getSkey() + transac[a1].getRkey() + transac[a1].getamount();
+				if (Hash(check.c_str(), check.length()) == transac[a1].getID())
+					transa1.push_back(transac[a1]);
+				check = transac[b1].getSkey() + transac[b1].getRkey() + transac[b1].getamount();
+				if (Hash(check.c_str(), check.length()) == transac[b1].getID())
+					transb1.push_back(transac[b1]);
+				check = transac[c1].getSkey() + transac[c1].getRkey() + transac[c1].getamount();
+				if (Hash(check.c_str(), check.length()) == transac[c1].getID())
+					transc1.push_back(transac[c1]);
+				check = transac[d1].getSkey() + transac[d1].getRkey() + transac[d1].getamount();
+				if (Hash(check.c_str(), check.length()) == transac[d1].getID())
+					transd1.push_back(transac[a1]);
+				check = transac[e1].getSkey() + transac[e1].getRkey() + transac[e1].getamount();
+				if (Hash(check.c_str(), check.length()) == transac[e1].getID())
+					transe1.push_back(transac[e1]);
 			}
 			int nonceas = 0;
+			int k = 0;
+			int saka = 1;
+			vector < vector<transaction>> transakcijos;
+			transakcijos.push_back(transa1);
+			transakcijos.push_back(transb1);
+			transakcijos.push_back(transc1);
+			transakcijos.push_back(transd1);
+			transakcijos.push_back(transe1);
+
+			unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+			std::array<int, 5> foo {0, 1, 2, 3, 4};
+			shuffle (foo.begin(), foo.end(), std::default_random_engine(seed));
+			int maxtries = 10000;
 			while (1)
 			{
-
-
+				k++;
+				int p = 0;
 				time_t now = time(0);
 				char* dt = ctime(&now);
 
-				block GenesisBlock("0", dt, "0", nonceas, diff, trans, TRANSNUMBER);
+				if (k % maxtries == 0)
+				{
+					p++;
+				}
+				if (p >4)
+				{
+					maxtries = maxtries * 10;
+					p = 0;
+				}
+				block GenesisBlock("0", dt, "0", nonceas, diff, transakcijos[foo[p]], TRANSNUMBER);
 				//std::ostringstream stream; // a stream is built
 				//stream << GenesisBlock;// the stream is filled
 				//string hashable = stream.str(); // we extract the contents of the stream
-				string hashable=GenesisBlock.ReturnHashable();
+				string hashable = GenesisBlock.ReturnHashable();
 				string blockHash = Hash(hashable.c_str(), hashable.length());
 				int nuliai = 0;
 				for (int i = 0; i < diff; i++)
@@ -229,13 +305,13 @@ void GenerateBlock(int blockNr, vector<user>& accounts, vector<transaction>& tra
 		}
 		else
 		{
-			cout<<"Jei norite testi bloku generavima spauskite bet koki mygtuka"<<endl;
+			cout << "Jei norite testi bloku generavima spauskite bet koki mygtuka" << endl;
 			cin.get();
 			vector <transaction> trans;
-			if (transac.size()<100)
-				cout<<"TRUKSTA TRANSAKCIJU"<<endl;
-			while(trans.size()<100)
-			{	
+			if (transac.size() < 100)
+				cout << "TRUKSTA TRANSAKCIJU" << endl;
+			while (trans.size() < 100)
+			{
 				trans.push_back(transac.back());
 				transac.pop_back();
 
@@ -247,12 +323,12 @@ void GenerateBlock(int blockNr, vector<user>& accounts, vector<transaction>& tra
 
 				time_t now = time(0);
 				char* dt = ctime(&now);
-				block temp=blockchain.back(); 
+				block temp = blockchain.back();
 				block GenesisBlock(temp.getprevHash(), dt, "0", nonceas, diff, trans, TRANSNUMBER);
 				//std::ostringstream stream; // a stream is built
 				//stream << GenesisBlock;// the stream is filled
 				//string hashable = stream.str(); // we extract the contents of the stream
-				string hashable=GenesisBlock.ReturnHashable();
+				string hashable = GenesisBlock.ReturnHashable();
 				string blockHash = Hash(hashable.c_str(), hashable.length());
 				int nuliai = 0;
 				for (int i = 0; i < diff; i++)
